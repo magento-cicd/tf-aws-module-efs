f108f982899eef82bd57c67aa6fcb1c532ca84a6
Chris Burton <5420465+burtino@users.noreply.github.com>
2019-05-10 17:53:47 -0500

    Update Test (test in dev)

--- examples/efs-default-settings/main.tf
+++ examples/efs-default-settings/main.tf
@@ -1,16 +1,22 @@
 #--------------------------------------
 # efs module default variables example
 # Notes: 
-# Creation ~ 3 minutes
-#  Destroy ~ 1 minute
+# Creation ~ 3-15 minutes
+#  Destroy ~ 1-10 minutes
 #--------------------------------------
 
 module "efs" {
   name = "efs-module-test-default"
 
+  kms_key_id = ""
+
   source = "../../modules/efs"
 
-  vpc_id = ""
+  vpc_id = "vpc-"
 
-  subnet_ids = [""]
+  subnet_ids = [
+    "subnet-",
+    "subnet-",
+    "subnet-",
+  ]
 }
